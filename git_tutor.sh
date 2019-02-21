eac5adbabee06ce06eb88e0c2eaaa8d185907906
LST512 <838395676@qq.com>
2019-02-21 10:42:16 +0800

    add more

--- git_tutor.sh
+++ git_tutor.sh
@@ -126,6 +126,10 @@ git pull origin master master
 '''
 git pull命令的作用是，取回远程主机某个分支的更新，再与本地的指定分支合并。
 '''
+git add . #他会监控工作区的状态树，使用它会把工作时的所有变化提交到暂存区，包括文件内容修改(modified)以及新文件(new)，但不包括被删除的文件。
 
+git add -u #他仅监控已经被add的文件（即tracked file），他会将被修改的文件提交到暂存区。add -u 不会提交新文件（untracked file）。（git add --update的缩写）
+
+git add -A #是上面两个功能的合集（git add --all的缩写）
 
 
